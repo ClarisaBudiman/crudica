f6cbb1b9e64f55b692d1e2cedffb4426df38b550
ClarisaBudiman <clarisaeirenebudiman@gmail.com>
2022-01-20 22:05:18 +0800

    hapus data

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
                                         umur = '$_POST[tumur]'
                                       WHERE no = '$_GET[id]'
                                      ");
-    if ($edit) // jika edit aukses
+    if ($edit) // jika ediukses
       {echo "<script>
               alert('edit Data Sukses!');
               document.location = 'index.php';
